66583f49cad39f8c73a527052428382ef480fa94
jcmexdev <carlos.garcia.esquivel@live.com.mx>
2019-09-29 13:50:23 -0500

    first challenge

--- src/index.js
+++ src/index.js
@@ -2,37 +2,72 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
+class Storage {
+  constructor(){
+    this.next = null;
+    this.key = 'next_fetch'
+    this.api = API
+  }
+
+  getCurrentApi() {
+      return this.isFirst() ? this.api : this.getNext()
+  }
+
+  isFirst() {
+    return localStorage.getItem(this.key) === null ? true : false
+  }
+
+  getNext() {
+    return localStorage.getItem(this.key)
+  }
+
+  setNext(value){
+    localStorage.setItem(this.key, value)
+  }
+
+  clearAll(){
+    localStorage.clear()
+  }
+}
+
+const store = new Storage()
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      store.setNext(response.info.next)
+      renderResults(response)
     })
     .catch(error => console.log(error));
+}
+
+const renderResults = ({results}) => {
+  const characters = results;
+    let output = characters.map(character => {
+      return `
+        <article class="Card">
+          <img src="${character.image}"/>
+          <h2>${character.name}<span>${character.species}</span></h2>
+        </article>
+    `
+    }).join('');
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
 }
 
 const loadData = () => {
   getData(API);
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
+const callBack = (entries) => {
   if (entries[0].isIntersecting) {
     loadData();
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+}
+
+const intersectionObserver = new IntersectionObserver( callBack, { rootMargin: '0px 0px 100% 0px',});
 
 intersectionObserver.observe($observe);
